8bccdacd20c5a8c1b5db3b8f73c1bc6e18b529de
dangtuan7193 <dangtuan7193@gmail.com>
2014-07-21 16:47:36 +0700

    Dang Van Tuan

--- HRMS/TryWebForm1.aspx.cs
+++ HRMS/TryWebForm1.aspx.cs
@@ -16,7 +16,7 @@ namespace HRMS
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            TextBox1.Text = "Tran Hai Nam";
+            TextBox1.Text = "Dang van tuan";
         }
     }
 }
